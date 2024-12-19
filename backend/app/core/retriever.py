82b119f475413e850a153da23a220a791df75469
Shivam Agarwal <99248722+shivamagarwal2510@users.noreply.github.com>
2024-12-19 13:06:06 +0530

    added retriever

---
+++ backend/app/core/retriever.py
@@ -0,0 +1,31 @@
+from langchain_openai import OpenAIEmbeddings
+from backend.app.core.vector_store import build_vector_store_from_args
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from backend.app.core.llm import build_llm_via_langchain
+from typing import Dict, List, Optional
+from backend.app.core.data_manager import DataManager
+
+def build_retriever_from_args(args, data_manager: Optional[DataManager] = None):
+    """Builds a retriever (with optional reranking) from command-line arguments."""
+
+    if args.embedding_provider == "openai":
+        embeddings = OpenAIEmbeddings(model=args.embedding_model)
+    # elif args.embedding_provider == "voyage":
+    #     embeddings = VoyageAIEmbeddings(model=args.embedding_model)
+    # elif args.embedding_provider == "gemini":
+    #     embeddings = GoogleGenerativeAIEmbeddings(model=args.embedding_model)
+    else:
+        embeddings = None
+    retriever = build_vector_store_from_args(args, data_manager).as_retriever(
+        top_k=args.retriever_top_k, embeddings=embeddings, namespace=args.index_namespace
+    )
+
+    if args.multi_query_retriever:
+        retriever = MultiQueryRetriever.from_llm(
+            retriever=retriever, llm=build_llm_via_langchain(args.llm_provider, args.llm_model)
+        )
+
+    # reranker = build_reranker(args.reranker_provider, args.reranker_model, args.reranker_top_k)
+    # if reranker:
+    #     retriever = ContextualCompressionRetriever(base_compressor=reranker, base_retriever=retriever)
+    return retriever
